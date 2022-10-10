cfa55e2416417e92edea78fe5650a52e83385ad8
Barton Rhodes <barton@bmorphism.xyz>
2022-10-10 03:53:26 -0400

    gmeow

---
+++ website/README.md
@@ -0,0 +1,15 @@
+# 🐈‍⬛ CatDAO: keep calm and purr on
+A good enough™️ landing page for cybernetic cats everywhere.
+Artisanally crafted in Portland, OR 🌳 @ Bobacat Tea House 🧋
+
+Live demo: [CatDAO](https://catdao.neocities.org)
+## Commands
+
+All commands are run from the root of the project, from a terminal:
+
+| Command           | Action                                       |
+| :---------------- | :------------------------------------------- |
+| `npm install`     | Installs dependencies                        |
+| `npm run dev`     | Starts local dev server at `localhost:3000`  |
+| `npm run build`   | Build your production site to `./dist/`      |
+| `npm run preview` | Preview your build locally, before deploying |
---
+++ website/src/config.mjs
@@ -0,0 +1,37 @@
+export const SITE = {
+	name: 'CatDAO',
+
+	origin: 'https://catdao.neocities.org',
+	basePathname: '/',
+
+	title: 'CatDAO — the cattiest DAO there ever was',
+	description: '🐈‍⬛ CatDAO is for loopy cat lovers everywhere',
+
+	googleAnalyticsId: false, // or "G-XXXXXXXXXX",
+	googleSiteVerificationId: 'orcPxI47GSa-cRvY11tUe6iGg2IO_RPvnA1q95iEM3M',
+};
+
+export const BLOG = {
+	disabled: false,
+	postsPerPage: 4,
+
+	blog: {
+		disabled: false,
+		pathname: 'blog', // blog main path, you can change this to "articles" (/articles)
+	},
+
+	post: {
+		disabled: false,
+		pathname: '', // empty for /some-post, value for /pathname/some-post 
+	},
+
+	category: {
+		disabled: false,
+		pathname: 'category', // set empty to change from /category/some-category to /some-category
+	},
+
+	tag: {
+		disabled: false,
+		pathname: 'tag', // set empty to change from /tag/some-tag to /some-tag
+	},
+};
